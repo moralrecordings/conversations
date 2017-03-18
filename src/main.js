d5aef1b576cde7e9282507b58705e55894ba8a28
Scott Percival <code@moral.net.au>
2017-03-18 12:47:06 +0800

    router working I guess?

--- src/main.js
+++ src/main.js
@@ -2,14 +2,23 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import base from './base';
+
+import conversations from './conversations';
+import login from './login';
 
 Vue.use(VueRouter);
 
+const routes = [
+    { path: '/conversations', component: conversations },
+    { path: '/', component: login }
+];
+
+const router = new VueRouter({
+    routes: routes   
+});
+
 global.conversations = new Vue({
     el: '#conversations',
-    template: '<mr-base/>',
-    components: { 
-        'mr-base': base
-    },
+    template: '<router-view/>',
+    router: router
 });
