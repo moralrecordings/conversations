643a40152b53608c8e65d3d6212c014079e9958c
Scott Percival <code@moral.net.au>
2017-03-25 20:17:51 +0800

    more emails

--- src/assets/emails.js
+++ src/assets/emails.js
@@ -64,8 +64,8 @@ export default [
         "subject": "URGENT - New business rule",
         "content": `
             <p>All,</p>
-            <p>Following a "lessons learned" analysis of last month's surprise loss in court, we are rolling out sweeping changes to our business procedures. In short, although an incident had been reported to the company through social media and a response issued, it was not escalated to the Customer Settlement Response team in time, by which point other related externalities had convinced the affected customer to file suit. We were unlucky that the plaintiff was unusually well resourced to bring about their case.</p>
-            <p>As a precaution to avoid this same confluence of events, we have added a new marker to the ticketing system. In future, please mark any messages wherein the author makes specific demands or legal threats to a business unit with the <b>"Litigious"</b> flag; these threads will be monitored by CSR for possible early settlement talks.</p>
+            <p>Following a "lessons learned" analysis of last month's surprise loss in court, we are rolling out sweeping changes to our business procedures. In summary: a customer incident was reported to the company through social media and a response issued, but was not escalated to the Customer Settlement Response team in time. As a result, other related externalities convinced the affected customer to file suit against the company. We were unlucky that this individual was unusually well resourced to bring about a case.</p>
+            <p>As a precaution to avoid this same confluence of events happening again, we have added a new marker to the ticketing system. In future, please mark any messages wherein the author makes specific demands or legal threats to a business unit with the <b>"Litigious"</b> flag; these threads will be monitored by CSR for possible early settlement talks.</p>
 
             <p><span style=\"color: darkblue; font-family: serif; line-height: 1em;\">Regards,<br/><br/><b>Nathaniel Hardcastle</b><br/>General Counsel - North America<br/><img src=\"`+ require('./logo.svg')+`\"><br/>111 Electric City Ave.<br/>Scranton, PA 18509<br/>Ph: 570-330-0000 (ext 1778)<br/>Fax: 570-330-9000<br/></span></p>
             <p style="font-family: serif">This email (including any attachments) is confidential and intended for the named recipient(s) only. It may be subject to legal or other professional privilege and contain copyright material. Any confidentiality or privilege is not waived or lost because this email has been sent to you by mistake.</p>
@@ -94,7 +94,7 @@ export default [
 
             <p>You probably don't know me, I used to be the social media manager for ABI. Sorry for the unsolicited email, I got your contact info from a friend still working for the Beast.</p>
 
-            <p>Basically, in case you haven't picked up on it there's no long-term prospects for the job. ABI hired some teenage fucks from the Bay Area to "train" some neural AI thing with what you're doing, which they intend to set loose on the public again as soon as they figure out how to make it stop talking like a Nazi. You probably heard about the last time their social media account was on the news? Yeah, that happened a few days after me and my team were fired and replaced with version 1. </p>
+            <p>Basically, in case you haven't picked up on it there's no long-term prospects for the job. ABI hired some teenage fucks from the Bay Area to "train" some neural AI thing with what you're doing, which they intend to set loose on the public again as soon as they figure out how to make it stop talking like a Nazi. You probably heard about the last time their social media account was on the news? Yeah, that happened a few days after me and my team got canned and replaced with version 1. </p>
 
             <p>What makes me fucking livid is this unpaid internship horseshit. Okay, the job wasn't great money, but it was at least a salary. I am 99% sure that their game plan is to ditch you at the end of the trial period and just churn people until they have enough data or whatever to switch CyberGoebbels back on. Obviously you're free to do as you wish, but I figure the least I can do is warn you about what's going down.</p>
 
@@ -126,9 +126,9 @@ export default [
         "content": `
             <p>Heya,</p>
 
-            <p>Thanks for replying back. I respect your decision, but I still think you're being played for a chump. Don't get me wrong, I do get that it's a really shit job market right now, but please promise me that you'll look for other positions. This isn't just me being bitter or the free work scam; Allied is seriously a fucked up place. The business is running at a loss, the leadership is an endless buck-passing circlejerk, and everyone is being pushed to cut corners. Jesus, they actually used the word "dissent" to justify cracking down on managers and quality control supervisors doing their job! I can't make this shit up.</p>
+            <p>Thanks for replying back. I respect your decision, but I still think you're being played for a chump. Don't get me wrong, I do get that it's a really shit job market right now, but please promise me that you'll look for other positions. This isn't just me being bitter; Allied is seriously a <i>fucked up</i> place. The business is running at a loss, the leadership is an endless buck-passing circlejerk, and everyone is being pushed to cut corners. Aggressive stack ranking has created a culture of undermining and snitching worthy of the Stasi. Jesus, they actually used the words "quash dissent" to justify cracking down on managers and quality control supervisors doing their job! I can't make this shit up.</p>
 
-            <p>But yes, lots of mistakes are being made and good people are being pressured to quit. I probably shouldn't go into old gossip, but in a nutshell I do not use Allied Brands products, nor would I recommend them to anyone wishing to live to a ripe old age. Mark my words, one day there will be a public scandal, and it will be big enough that your job description will change to "human sewer rake". I would not wish that horrorshow on anyone.</p>
+            <p>But yes, lots of mistakes are being made and good people are being pressured to quit. I probably shouldn't go into old gossip, but in a nutshell I do not use Allied Brands products, nor would I recommend them to anyone wanting to live to a ripe old age. Mark my words; one day there will be a public scandal, and it will be big enough that your job description will change to "human sewer rake". I would not wish that horrorshow on anyone.</p>
 
             <p>Cheers,<br/>Andrea</p>
         `
@@ -136,6 +136,30 @@ export default [
 
 
     // week 5 starting 20111128
+    {
+        "date": moment("20111124", "YYYYMMDD"),
+        "sender": "MediaMunch Alerts",
+        "senderMail": "alerts@mediamunch",
+        "recipient": "Me",
+        "recipientMail": "againstpollution@ichiromail",
+        "subject": "New Article: Beaufort issues nationwide recall amid tainted milk scandal",
+        "content": `
+            <p style="font-size: 1.5rem;">News alerts for "allied brands":</p>
+            <div style="border-radius: 8px; border: 1px solid grey; padding-left: 1em; padding-right: 1em;">
+                <h2 style="margin-bottom: 0;">Beaufort issues nationwide recall amid tainted milk scandal</h2>
+                <p style="font-variant: small-caps">Robert Xam in Chicago</p>
+
+                <p>Beaufort North America LLC. has issued an immediate recall for its Healthland Farms milk product range, following widespread reports of children suffering from impaired kidney functionality.</p>
+                <p>Since Wednesday, there has been a spike in diagnosed cases of sudden-onset kidney trauma; an uncommon renal disorder affecting pre-teen children. The majority of cases have been traced back to the consumption of Healthland Farms Low-Fat Chocolate Milk; a flavoured milk drink produced by Beaufort and supplied to the National School Lunch Program. Unconfirmed reports suggest that batches of milk were tainted by a melamine-like additive to boost the apparent protein content, similar to the Chinese milk adulteration scandal in 2008.</p>
+                <p>A spokesperson for Beaufort stated that that a nationwide recall was a necessary safety precaution, the company was "shocked and dismayed" at this turn of events, and that their investigation into the incident was still ongoing. Customers who believe they are affected by the recall are urged not to consume the product and to get in contact with Beaufort.</p>
+                <p>The market response to the scandal has been severe, with Beaufort stock down $3.50 to $9.02.</p>
+                <p>In September, Beaufort finalized their acquisition of several product lines from Pennsylvania-based conglomerate Allied Brands Inc., including Healthland Farms Milk. At the time, the milk product business was considered healthy and no concerns were raised, aside from a lone activist investor demanding a higher premium on the market price.<p>
+                <p>A spokesperson for Allied Brands Inc. declined to comment, citing the complete divestment of the product line. The Allied stock price suffered some early losses when news of the scandal broke on Wednesday, but has since rallied to be slightly ahead by close of trading today.</p>
+            </div>
+            <p style="text-align: center;"><small>© 2011 MediaMunch Holdings.<br/>To adjust your notification preferences, visit the website.</small></p>
+
+        `
+    },
 
     {
         "date": moment("20111124", "YYYYMMDD"),
@@ -154,10 +178,69 @@ export default [
     },
 
 
+    {
+        "date": moment("20111205", "YYYYMMDD"),
+        "sender": "Tricia Huxton",
+        "senderMail": "tricia.huxton@alliedbrandsinc",
+        "recipient": "PR - L1",
+        "recipientMail": "pr_l1@alliedbrandsinc",
+        "subject": "Good news story - relaunch of Shaftoproxin®",
+        "content": `
+            <p>Hi team,</p>
+            <p>There's been some good news from our medical division Excelsior Pharmaceuticals. Shaftoproxin® is a pharmaceutical product originally developed as a treatment for stump rash, boatswain's disease, deep vein atherosclerosis and night prowls. Thanks to some diligent work by our excellent pathology team Shaftoproxin® has been proven effective in relieving the symptoms of sudden onset kidney trauma, and as the product has existing FDA approval, Excelsior is first to market with such a treatment.</p>
+            <p>In order to best engage customers about the benefits of Shaftoproxin®, the company plans to launch an awareness campaign along with updated product packaging and marketing materials, plus a wider rollout to retailers to meet anticipated demand. As part of this push, the incoming feed will now include messages from prospective customers who have posted about sudden onset kidney trauma. Please engage as many prospective customers as possible to recommend Shaftoproxin® from the Excelsior Pharmaceuticals (<b>@excelsior_rx</b>) account.</p>
+
+            <p><span style=\"color: darkblue; font-family: serif; line-height: 1em;\">Kind regards,<br/><b>Tricia Huxton</b><br/>Public Relations Coordinator<br/><img src=\"`+ require('./logo.svg')+`\"><br/>111 Electric City Ave.<br/>Scranton, PA 18509<br/>Ph: 570-330-0000 (ext 3519)<br/>Fax: 570-330-9000</span><br/><br/><small style="color: darkgreen; font-family: serif; line-height: 1em;">🌳&nbsp;&nbsp; Please consider your carbon footprint before printing this email<small></p>
+        `
+    },
+
+
+    {
+        "date": moment("20111209", "YYYYMMDD"),
+        "sender": "Cody Briggs",
+        "senderMail": "cody.briggs@alliedbrandsinc",
+        "recipient": "PR - L1",
+        "recipientMail": "pr_l1@alliedbrandsinc",
+        "subject": "CONFIDENTIAL - Talking points for Shaftoproxin® product refresh",
+        "content": `
+        <p>All,</p>
+        <p>You may have seen some negative reports in the media concerning Excelsior Pharmaceuticals and the recent product refresh for Shaftoproxin®. At this time of increased scrutiny it is of utmost importance that we remain on-message and correct any sensationalist misconceptions made in the tabloid press. For your reference, here are the current approved set of talking points regarding Shaftoproxin®:</p>
+        <p><b>Why has the price of the product changed?</b></br>
+        The prices of all Excelsior Pharmaceuticals products have changed over time to better reflect important features and the value the product provides.</p>
+        <p><b>How come the price of the drug in America is different compared to other countries?</b><br/>Since acquiring the Shaftoproxin® product line, we have worked diligently and invested significantly to enhance Shaftoproxin® and make it more available. We continue to invest in product improvements, such as a formulation with a longer shelf life that would extend the period between refills.</p>
+        <p><b>The new price is too expensive.</b><br/>A drug's list price is not the primary factor in determining patient affordability and access. A reduction in the list price of Shaftoproxin® would not translate into a benefit for patients. We are working with hospitals and healthcare providers to get every patient covered, including free-of-charge options for uninsured patients and co-payment assistance programs.</p>
+        <p><b>Your company is denying sick people access to vital medicine.</b><br/>Our priority continues to be supporting our healthcare providers and ensuring their patients who need Shaftoproxin® have ready and affordable access to it. We pledge that no patient needing Shaftoproxin® will ever be denied access.</p>
+        <p><b>Your company is gouging customers with this new price.</b><br/>For sudden onset kidney trauma and other critical, under-treated diseases, the status quo is not an option. Excelsior hopes to change that by targeting investments that both improve on the current formulation and seek to develop new therapeutics with better clinical profiles that we hope will help eradicate the disease.</p>
+
+            <p><span style=\"color: darkblue; font-family: serif; line-height: 1em;\">Regards,<br/><b>Cody Briggs</b><br/>Chief Media Officer<br/><img src=\"`+ require('./logo.svg')+`\"><br/>111 Electric City Ave.<br/>Scranton, PA 18509<br/>Ph: 570-330-0000 (ext 4816)<br/>Fax: 570-330-9000</span><br/><br/><small style="color: darkgreen; font-family: serif; line-height: 1em;">🌳&nbsp;&nbsp; Please consider your carbon footprint before printing this email<small></p>
+        `
+    },
+
+
+
+
+
+
+
+    {
+        "date": moment("20111212", "YYYYMMDD"),
+        "sender": "Roger Billeton",
+        "senderMail": "roger.billeton@alliedbrandsinc",
+        "recipient": "All Users",
+        "recipientMail": "pr_all@alliedbrandsinc",
+        "selfDestruct": 8000,
+        "subject": "Re: FW: IMPORTANT - New branding strategy for Kingsley's Fruit Pebbles",
+        "content": `
+            <p>someone tell Kate's team that they better get on Board with this shit if they want jobs after Christmas</p>
+
+<p><span style=\"color: darkblue; font-family: serif; line-height: 1em;\"><b>Roger Billeton</b><br/>Chief Executive Officer<br/><img src=\"`+ require('./logo.svg')+`\"><br/>111 Electric City Ave.<br/>Scranton, PA 18509<br/>Ph: 570-330-0000 (ext 1119)<br/>Fax: 570-330-9000<br/></span></p>
+            <p style="font-family: serif">This email (including any attachments) is confidential and intended for the named recipient(s) only. It may be subject to legal or other professional privilege and contain copyright material. Any confidentiality or privilege is not waived or lost because this email has been sent to you by mistake.</p>
+        `,
+    },
 
     // week ???
     {
-        "date": moment("20111107", "YYYYMMDD"),
+        "date": moment("20111219", "YYYYMMDD"),
         "sender": "Nathaniel Hardcastle",
         "senderMail": "nathaniel.hardcastle@alliedbrandsinc",
         "recipient": "PR - All Users",
@@ -166,7 +249,7 @@ export default [
         "content": `
             <p>All,</p>
             
-            <p>It has come to the attention of the Legal team that a small number of Excelsior Pharmaceuticals customers are expressing despair at a perceived inability to procure our products for themselves or their loved ones. Without question, the welfare of all our customers should come first and foremost in everything we do. As good corporate citizens and as Americans, we have a duty to look beyond what is best for our image or bottom line, and provide help to those who need it the most. It's time for us to open up our moral compass and chart a new, righteous course.</p>
+            <p>It has come to the attention of Legal that a small number of Excelsior Pharmaceuticals customers are expressing despair at a perceived inability to procure our products for themselves or their loved ones. Without question, the welfare of all our customers should come first and foremost in everything we do. As good corporate citizens and as Americans, we have a duty to look beyond what is best for our image or bottom line, and provide help to those who need it the most. It's time for us to open up our moral compass and chart a new, righteous course.</p>
 
             <p>For any customer you would identify as "<b>at-risk</b>", please refer them to the National Suicide Prevention Lifeline for assistance. This should be done from the Allied Brands Inc. corporate account (<b>@alliedbrandsinc</b>) ONLY, so as not to conflate this serious issue with the light-hearted tone used for advertising/social media outreach by the individual brands.</p>
 
